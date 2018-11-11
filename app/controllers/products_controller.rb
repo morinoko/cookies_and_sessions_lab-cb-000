d8b24e96df6e70ca164347108ced5fc49cdbe9b3
Felice Forby <feliceforby@gmail.com>
2018-11-11 23:15:48 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,8 +4,9 @@ class ProductsController < ApplicationController
   end
 
   def add_to_cart
+    @cart = cart
     item = params[:item_name]
-    cart << item
+    @cart << item
 
     redirect_to root_path
   end
